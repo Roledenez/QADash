8e99534e1fe19928ead9626f8facf04524e79a3f
Binalie Ravinga <binalie3@yahoo.com>
2015-09-11 07:02:14 -0700

    testsuitesDetails model class commit

---
+++ application/models/testsuiteDetails_model.php
@@ -0,0 +1,46 @@
+<?php
+class testsuiteDetails_model extends My_Model {
+
+    public function __construct(){
+	parent::__construct();                       
+    }
+
+    public function getTestsuitesDetails($id) {
+        
+        $sql_1 = "select ts.testsuites_id as testsuitesID,"
+                . " ts.project_id as projectID,"
+                . " ts.name as testsuiteName,"
+                . " ts.description as testsuiteDescription,"
+                . " ts.status as testsuiteStatus,"
+                . " count(tc.testcase_id) as numTestcases  "
+                . "from testsuites ts, testcase tc "
+                . "where ts.testsuites_id = tc.testsuites_id "
+                . "and ts.project_id = $id"; 
+
+        
+        return $this->db->query($sql_1, array($id))->result();
+    } 
+    
+    public function getClosedTestcases($id){     
+
+        //$sql_2 = "select count(tc.testcase_id) as numClosedTestcases from testcase tc, testsuites ts where ts.testsuites_id = tc.testsuites_id and ts.project_id = $id and tc.status = 0";
+        $sql_2 = "select count(tc.testcase_id) as numClosedTestcases "
+                . "from testcase tc, testsuites ts "
+                . "where ts.testsuites_id = tc.testsuites_id and "
+                . "ts.project_id = $id and tc.status = 0";
+        
+        return $this->db->query($sql_2, array($id))->result();
+    }
+    
+    public function getOpenTestcases($id){
+       
+        //$sql_3 = "select count(tc.testcase_id) as numOpenTestcases from testcase tc, testsuites ts where ts.testsuites_id = tc.testsuites_id and ts.project_id = $id and tc.status = 1";
+        $sql_3 = "select count(tc.testcase_id) as numOpenTestcases "
+                . "from testcase tc, testsuites ts "
+                . "where ts.testsuites_id = tc.testsuites_id and "
+                . "ts.project_id = $id and tc.status = 1";
+        
+        return $this->db->query($sql_3, array($id))->result();
+    }
+    
+}
